06d49ae2a7100b516e8406ea69c8a8c37ce2b1bf
Ihsaan Bhamjee <bhamjeei@legal.regn.net>
2024-10-14 11:22:29 +0200

    create login form

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helpers";
+import Intro from "../components/Intro";
+import { toast } from "react-toastify";
 
 interface DashboardLoaderData {
     userName: string;
@@ -10,13 +12,24 @@ export function dashboardLoader(): DashboardLoaderData {
     return { userName }
 }
 
+export const dashboardAction = async ({request} : {request: Request}) => {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data);
+    try {
+        localStorage.setItem("userName", JSON.stringify(formData.userName));
+        return toast.success(`Welcome, ${formData.userName}`);
+    }
+    catch(e: unknown) {
+        throw new Error("There was a problem creating your account.");
+    }
+}
+
 const Dashboard = () => {
     const { userName } = useLoaderData() as DashboardLoaderData
     return (
-        <div>
-            <h1>{userName}</h1>
-            Dashboard
-        </div>
+        <>
+            {userName ? (<p>{userName}</p>) : <Intro/> }
+        </>
     )
 }
 
