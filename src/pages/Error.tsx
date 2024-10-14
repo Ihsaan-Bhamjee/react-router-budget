1fac280d376e89766c30543af7b9d00329acd4d2
Ihsaan Bhamjee <bhamjeei@legal.regn.net>
2024-10-14 11:33:45 +0200

    handle display of custom error messages

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,36 @@
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
+import { Link, useNavigate, useRouteError } from "react-router-dom";
+
+type RouteError = {
+    message?: string;
+    statusText?: string;
+    status?: number;
+  };
+
 const Error = () => {
+    const error = useRouteError() as RouteError;
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Error
+        <div className="error">
+            <h1>Uh oh! We've got a problem</h1>
+            <p>{error.message || error.statusText}</p>
+            <div className="flex-md">
+                <button 
+                    className=" btn btn--dark"
+                    onClick={() => navigate(-1)}
+                >
+                    <ArrowUturnLeftIcon width={20} />
+                    <span>Go back</span>
+                </button>
+                <Link
+                    to={"/"}
+                    className="btn btn--dark"
+                >
+                    <HomeIcon width={20} />
+                    <span>Go home</span>
+                </Link>
+            </div>
         </div>
     )
 }
